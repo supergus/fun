0d850f2ce46191fbb53decc770e8de6bfb705c05
Christopher Couch <33438131+supergus@users.noreply.github.com>
2020-11-22 10:19:22 -0500

    Howdy

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
 
     # Mandatory fields
     name='fun-supergus',
-    version='0.1.2',
+    version='0.1.3',
     packages=setuptools.find_packages(),    # Builds & wheels are limited to pkgs specified here
 
     # Optional fields
