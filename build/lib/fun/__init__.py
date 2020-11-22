f16ece0005a902f7a1c9531fb9f1949b6bdbaa54
Christopher Couch <33438131+supergus@users.noreply.github.com>
2020-11-22 10:18:36 -0500

    Howdy

--- build/lib/fun/__init__.py
+++
@@ -1,9 +0,0 @@
-"""This is the mostest awesomeset package EVAR."""
-
-__author__ = "Christopher Couch"
-__license__ = "Strictly proprietary for Liveline Technologies, Inc."
-__version__ = "2020-11"
-
-
-from fun.printing.formatted_console_print import fancy_print
-from fun.communications import communicator as et
--- scratch.py
+++
@@ -1,3 +0,0 @@
-import fun
-
-fun.et.phone_home('howdy', 7345454764)
